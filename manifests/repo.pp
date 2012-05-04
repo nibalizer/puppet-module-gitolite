05c0b90a280c448d291bbb02103e25bf37031b5f
Ben Kero <ben.kero@gmail.com>
2012-05-03 20:56:12 -0700

    add rwplus, rw, r params to repo type

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -2,6 +2,9 @@ define gitolite::repo ($path,
                        $owner='Mozilla',
                        $desc='This repo needs a description',
                        $order='999',
+                       rwplus='',
+                       rw='',
+                       r=''
                        $hooks=[]) {
     file {"${title}":
         ensure  => present,

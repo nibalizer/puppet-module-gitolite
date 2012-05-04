41f42fc49109d39415d5f0a9f9f11e28b20329c2
Nibalizer <krum.spencer@gmail.com>
2012-05-04 04:21:12 -0700

    Duplicated execs from config.pp into repo.pp

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -10,6 +10,26 @@ define gitolite::repo ($owner='Mozilla',
         path    => "${gitolite::root}/.gitolite.conf.d/${order}-${title}",
         content => template('gitolite/repo.erb'),
         mode    => '0644',
-        notify  => Exec['gitolite::config::generate-repo-list']
+        notify  => Exec["generate-repo-list-${name}"]
+    }
+
+    exec {
+        "generate-repo-list-${name}":
+            cwd => "$gitolite::root/.gitolite.conf.d",
+            command => '/usr/local/bin/concat-gl-conf',
+            user => $gitolite::user,
+            require => [File['concat-gl-conf'], File['gl-conf-dir']],
+            environment => "HOME=$gitolite::root",
+            notify => Exec['update-conf'],
+            refreshonly => true;
+        "update-conf-${name}":
+            cwd => "$gitolite::root/.gitolite",
+            command => '/usr/bin/gl-compile-conf',
+            user => $gitolite::user,
+            environment => ["HOME=$gitolite::root",
+                            'GL_BINDIR=/usr/bin',
+                            "GL_RC=$gitolite::root/.gitolite.rc"],
+            refreshonly => true;
+
     }
 }
